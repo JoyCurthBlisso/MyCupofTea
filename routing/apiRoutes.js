f04235571bb49b93fe139a876325ea325566e288
Joy Curth <joy.curth@comcast.net>
2018-03-26 18:01:38 -0400

    fixed bugs

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,29 +2,38 @@ var path = require('path');
 var cupoftea = require("../app/data/cupoftea.js")
 
 
-module.exports = function(app){
+module.exports = function (app) {
 
-  app.get('/api/cupoftea', function(req,res){
-    res.json(cupoftea);
-  });
+    app.get('/api/cupoftea', function (req, res) {
+        res.json(cupoftea);
+    });
 
-  app.post('/api/cupoftea', function(req,res){
-    
-     var lowestDiff = 50;
-        var mycupoftea;
+    app.post('/api/cupoftea', function (req, res) {
+        console.log(req.body);
+        var lowestDiff = 50;
+        var match;
         cupoftea.forEach(function (teascores) {
-            var diff = 0;
-            for (i = 0; i < teascores.scores.length; i++) {
-                diff += Math.abs(teascores.scores[i] - req.body.scores[i]);
-            } 
-            if (difference < lowestDiff) {
-                lowestDiff = diff;
-                mycupoftea = teascores;
-            }
- 
-    
-   });
 
-    res.json(mycupoftea);
-  	cupoftea.push(req.body);
- };
+            // console.log(teascores);
+
+            var diff = 0;
+
+            for (var i = 0; i < 10; i++) {
+                diff += Math.abs(teascores.herbMatch[i] - req.body.scores[i]);
+            }
+            console.log(diff);
+
+            if (diff < lowestDiff) {
+
+                lowestDiff = diff;
+                match = teascores;
+
+            }
+
+        });
+        console.log(match);
+        cupoftea.push(req.body);
+        res.json(match);
+
+    });
+}
